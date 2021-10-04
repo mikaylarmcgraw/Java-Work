303f40af7490d3eb3f05acfb2719797767de7173
mikaylarmcgraw <mikayla.mcgraw@sage.com>
2021-10-04 09:42:42 -0700

    Adding work to almost perfect path

--- almost-shortest-path/src/AlmostShortestPath.java
+++ almost-shortest-path/src/AlmostShortestPath.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class AlmostShortestPath extends Node
 {
     // initalizing variables
-    private static int[] NodeArray;
+    private static Node[] NodeArray;
     private int[] Heap;
     private int currentSize;
     private int maxHeapSize;
@@ -24,12 +24,24 @@ public class AlmostShortestPath extends Node
         input = scanner.nextLine();
         startNodeEndNodeInitalize(input);
         
-        //initalize node array with all points before assigning neighbors
-        setNodeArray();
+        //building out node array
+        setNodeArrayLength();
         
+        //populate array of nodes on the map
+        for(int i = 0; i < NodeArray.length; i++)
+        {
+            addNodeToArray(i);
+            //debugging purposes print all values
+            System.out.println(NodeArray[i].getNodeValue());
+        }
+        
+        //assign neighbors
         for(int i = 0; i < numberOfEdges; i++)
         {
-            
+            int x;
+            int y;
+            int z;
+            assignNeighbors(x, y, z);
         }
     }
     
@@ -49,12 +61,21 @@ public class AlmostShortestPath extends Node
         endNode = Integer.parseInt(stringValue[1]);
     }
     
-    public static void setNodeArray()
+    public static void setNodeArrayLength()
     {
-        NodeArray = new int[numberOfNodes-1];
-        for (int i = 0; i <= numberOfNodes-1; i++)
-        {
-            NodeArray[i] = i;
-        }
+        NodeArray = new Node[numberOfNodes];
+    }
+    
+    public static void addNodeToArray(int nodeValue)
+    {
+            Node myNewNode = new Node();
+            myNewNode.setNodeValue(nodeValue);
+            NodeArray[nodeValue] = myNewNode;    
+    }
+    
+    public static void assignNeighbors(int nodeValue, int neighborValue, int distance)
+    {
+        
+        
     }
 }
