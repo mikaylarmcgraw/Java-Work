104303e427930aa01fb81c03ecdd2a91b0186cc6
mikaylarmcgraw <mikayla.mcgraw@sage.com>
2021-10-05 09:11:43 -0700

    Adding changes to almost shortest path

--- almost-shortest-path/src/Node.java
+++ almost-shortest-path/src/Node.java
@@ -5,6 +5,18 @@ public class Node
    private Hashtable<Integer, Integer> neighbors = new Hashtable<>();
    private boolean visited = false;
    private int distance = Integer.MAX_VALUE;
+   private int prevNode = Integer.MAX_VALUE;
+   
+   public void setPrevNode(int value)
+   {
+       this.prevNode = value;
+   }
+   
+   public int getPrevNode()
+   {
+       return this.prevNode;
+   }
+   
    public void setNodeId(int nodeId)
    {
        this.Id = nodeId;
